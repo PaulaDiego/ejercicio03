0897b26d9a7f34cd35a7aeecd7da62a1081f9084
paulacurso <paulacursocic@gmail.com>
2017-07-05 17:59:48 +0200

    Primer commit

---
+++ spec/test.js
@@ -0,0 +1,14 @@
+var modulo = require('../index.js');
+
+describe("A suite is just a function", function() {
+  var a;
+
+  it("and so is a spec", function() {
+    resultado = modulo.sumar(4,5);
+
+    expect(resultado).toBe(9);
+  });
+});
+    
+
+
